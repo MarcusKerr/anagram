dd400671000d2812e6e4524c0a33aa34df5a9c05
Marcus <marcus.kerr94@gmail.com>
2019-01-22 21:17:48 +0000

    Unit: word extractor spec, build bank, raises error if invalid file - RED

--- lib/Anagram.rb
+++ lib/Anagram.rb
@@ -5,8 +5,8 @@ class Anagram
   def initialize(word_extractor = WordExtractor.new)
     @word_extractor = word_extractor
   end
-  
+
   def upload(filename)
     @word_extractor.build_bank(filename)
-  end 
+  end
 end
--- spec/word_extractor_spec.rb
+++ spec/word_extractor_spec.rb
@@ -9,5 +9,9 @@ describe WordExtractor do
       word_extractor.build_bank('test_words.txt')
       expect(word_extractor.words[0]).to eq 'dog'
     end
+
+    it('raises error if file cannot be found') do
+      expect { word_extractor.build_bank('no file') }.to raise_error('This file does not exist')
+    end
   end
 end
