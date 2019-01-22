bd64508076252688aabc9e29595edd2a58dec0ad
Marcus <marcus.kerr94@gmail.com>
2019-01-22 19:17:11 +0000

    Unit: word extractor, build bank, returns array of words - RED

---
+++ spec/word_extractor_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'WordExtractor'
+
+describe WordExtractor do
+  let(:word_extractor) { subject }
+  describe '#build_bank' do
+    it('returns array of words') do
+      word_extractor.build_bank('test_words.txt')
+      expect(word_extractor.words[0]).to eq 'dog'
+    end
+  end
+end
