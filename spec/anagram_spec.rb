bb1ba23197da5690c50e4b6c27c4b1eeb3a31ec3
Marcus <marcus.kerr94@gmail.com>
2019-01-22 20:14:33 +0000

    Unit: anagram spec, upload, delegates to word extractor - RED

---
+++ spec/anagram_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# frozen_string_lieral: true
+
+require 'anagram'
+
+describe Anagram do
+  let(:mock_word_extractor_instance) { double :word_extractor_instance, :build_bank }
+  let(:mock_file_path) { 'filepath.txt' }
+  let(:anagram) { described_class.new(mock_word_extractor_instance) }
+
+  describe '#upload' do
+    it 'delegates to Word Extractor' do
+      expect(mock_word_extractor_instance).to receive(:build_bank).with(mock_file_path)
+      anagram.upload(mock_file_path)
+    end
+  end
+end
