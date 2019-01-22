83e3f3c59e909940ec69f5d85cdd909851f51002
Marcus <marcus.kerr94@gmail.com>
2019-01-22 18:40:11 +0000

    Feature: user can upload a file - RED

---
+++ spec/features/accept_input_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'Anagram'
+
+describe Anagram do
+  let(:anagram) { subject }
+
+  it 'allows the user to upload a file' do
+    expect { anagram.upload('test_words.txt') }.to_not raise_error
+  end
+end
