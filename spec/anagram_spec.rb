d8a868b0437868d9c1f31b4c68d10eab9347e4d4
Marcus <marcus.kerr94@gmail.com>
2019-01-23 01:22:09 +0000

    Unit: anagram spec, mock words array

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,7 +3,8 @@
 require 'anagram'
 
 describe Anagram do
-  let(:mock_word_extractor_instance) { double :word_extractor_instance }
+  let(:mock_word_extractor_instance) { double :word_extractor_instance, build_bank: mock_words_array, words: mock_words_array}
+  let(:mock_words_array) { ["dog", "goat", "fish"] }
   let(:file_path) { 'test_words.txt' }
   let(:anagram) { described_class.new(mock_word_extractor_instance) }
 
@@ -17,7 +18,7 @@ describe Anagram do
   describe '#find' do
     it 'returns a list of matched words' do
       anagram.upload_words(file_path)
-      expect(anagram.find('dog')).to eq ['god']
+      expect(anagram.find('god')).to eq ['dog']
     end
   end
 end
