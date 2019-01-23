647c371346564295fd5b9f806b785fda5db9aa40
Marcus <marcus.kerr94@gmail.com>
2019-01-23 01:33:28 +0000

    Refactor: anagram, find -> setdefaults

--- lib/Anagram.rb
+++ lib/Anagram.rb
@@ -2,8 +2,6 @@
 
 require_relative 'word_extractor'
 class Anagram
-  attr_reader :matched_words
-
   def initialize(word_extractor = WordExtractor.new)
     @word_extractor = word_extractor
   end
@@ -13,22 +11,29 @@ class Anagram
   end
 
   def find(word)
-    @matched_words = []
-    word = word.downcase
-    starting_index = @word_extractor.words.index { |word_item| word_item.length == word.length }
-    ending_index = @word_extractor.words.index { |word_item| word_item.length == word.length + 1 } || @word_extractor.words.length - 1
-    word_num = get_numerical_value(word)
-    while starting_index <= ending_index
-      this_word_num = get_numerical_value(@word_extractor.words[starting_index])
-      @matched_words << @word_extractor.words[starting_index] if this_word_num == word_num
-      starting_index += 1
+    set_defaults(word)
+    while @starting_index <= @ending_index
+      this_word_num = get_numerical_value(@word_extractor.words[@starting_index])
+      @matched_words << @word_extractor.words[@starting_index] if this_word_num == @word_num
+      @starting_index += 1
     end
     matched_words
+  end
+
+  private 
+
+  attr_reader :matched_words
+
+  def set_defaults(word)
+    @matched_words = []
+    word = word.downcase
+    @starting_index = @word_extractor.words.index { |word_item| word_item.length == word.length }
+    @ending_index = @word_extractor.words.index { |word_item| word_item.length == word.length + 1 } || @word_extractor.words.length - 1
+    @word_num = get_numerical_value(word)
   end
 
   def get_numerical_value(word)
     word.unpack('U*').sum
   end
 
-  private :matched_words, :get_numerical_value
 end
