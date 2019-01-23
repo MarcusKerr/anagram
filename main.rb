9178c6443e6306e9253f7f5f4d8b3a2637571f53
Marcus <marcus.kerr94@gmail.com>
2019-01-23 13:25:11 +0000

    refactor: main.rb

--- main.rb
+++ main.rb
@@ -3,22 +3,40 @@
 require './lib/anagram'
 
 anagram = Anagram.new
+STOPPING_CONDITION = 'STOP'
 
-puts 'Enter a file of words:'
-filename = gets.chomp
-anagram.upload_words(filename)
+def enter_file
+  loop do
+    puts "Enter a file of words (to exit enter 'STOP'):"
+    filename = gets.chomp
+    break if filename == STOPPING_CONDITION
 
-loop do
-  puts "\nEnter your anagram (to exit enter 'STOP'):"
-  word = gets.chomp
-  break if word == 'STOP'
-
-  matched_words = anagram.find(word)
-rescue Exception => e
-  puts e
-else
-  puts "\nMatched anagrams of '#{word}' are:\n"
-  matched_words.each do |word|
-    puts word
+    begin
+      anagram.upload_words(filename)
+    rescue Exception => e
+      puts e
+    else
+      enter_word
+      break
+    end
   end
 end
+
+def enter_word
+  loop do
+    puts "\nEnter your anagram (to exit enter 'STOP'):"
+    word = gets.chomp
+    break if word == STOPPING_CONDITION
+
+    matched_words = anagram.find(word)
+  rescue Exception => e
+    puts e
+  else
+    puts "\nMatched anagrams of '#{word}' are:\n"
+    matched_words.each do |word|
+      puts word
+    end
+  end
+end
+
+enter_file
